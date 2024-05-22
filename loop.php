4142fb7b2c943feaa7c3cd78fec155607136ef96
SumeyaMolu <sumeya.molu@strathmore.edu>
2024-05-22 08:24:12 +0300

    Added a functionality

---
+++ loop.php
@@ -0,0 +1,27 @@
+<h1>Loops</h1>
+
+<?php
+//while loop
+
+$min = 0;
+while($min <10){
+    print $min . "<br>";
+    $min++;
+}
+?>
+<h4>do-while loop</h4>
+<?php
+//do-while loop
+$x = 14;
+do{
+    print $x . "<br>";
+    $x++;
+}while($x < 7);
+?>
+<h4>for loop</h4>
+<?php
+    //for loop
+    for($r=3; $r<12; $r++){
+        print $r ."<br>";
+    }
+?>
