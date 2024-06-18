85e16d82571cdfb5d2476d352350e088c749a786
SumeyaMolu <sumeya.molu@strathmore.edu>
2024-06-18 12:12:46 +0300

    Added a functionality

--- People.php
+++ People.php
@@ -1,42 +1,7 @@
 <?php include_once("templates/headings.php"); ?>
 <body>
-    <div class="topnav"></div>
-    <a href="index.html">Home</a>
-    <a href="About.html">AboutUs</a>
-    <a href="Branches.html">Our Branches</a>
-    <a href="Deliveries.html">Deliveries</a>
-    <a href="Reviews.html">Reviews</a>
-    <a href="ContactUs.html">ContactUs</a>
-    <a href="Offers.html">CrazyOffers</a>
-    <a href="Box.html">Box</a>
-    <a href="People.html">People</a>
-    <a href="SignIn.html">Sign In</a>
-    <div  class ="topnav-right">
-        <a class="btn btn-secondary dropdown-toggle" 
-           href="#" 
-           role="button" 
-           id="dropdownMenuLink"
-           data-bs-toggle="dropdown" 
-           aria-expanded="false">
-            =
-        </a>
-
-    <ul class="dropdown-menu" 
-                aria-labelledby="dropdownMenuLink">
-                <li><a class="dropdown-item" href="signin.html "  >
-                     signIn
-                    </a>
-                </li>
-                <li><a class="dropdown-item" href="signUp.html">
-                        signUp
-                    </a>
-                </li>
-              
-            </ul>
-        </div>
-     </div>
-
-
+    <?php include_once("tempolates/nav.php"); ?>
+    <div class ="banner">
 
     <h1>Our Team</h1>
         </div>
