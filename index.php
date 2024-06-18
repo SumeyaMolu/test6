896eb390a46e4f8d86b8bfcfb21a6fdebea04fbb
SumeyaMolu <sumeya.molu@strathmore.edu>
2024-06-18 12:46:38 +0300

    Added a functionality

--- index.php
+++ index.php
@@ -1,59 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href=
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-            rel="stylesheet"
-            integrity=
-    "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
-            crossorigin="anonymous">
-        <script src=
-    "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src=
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity=
-    "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-            crossorigin="anonymous"></script>
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css"
-</head>
+<?php include_once("templates/headings.php"); ?>
 <body>
-    <div class="topnav"></div>
-    <a href="index.html">Home</a>
-    <a href="About.html">AboutUs</a>
-    <a href="Branches.html">Our Branches</a>
-    <a href="Deliveries.html">Deliveries</a>
-    <a href="Reviews.html">Reviews</a>
-    <a href="ContactUs.html">ContactUs</a>
-    <a href="Offers.html">CrazyOffers</a>
-    <a href="Box.html">Box</a>
-    <a href="SignIn.html">Sign In</a>
-    <div  class ="topnav-right">
-        <a class="btn btn-secondary dropdown-toggle" 
-           href="#" 
-           role="button" 
-           id="dropdownMenuLink"
-           data-bs-toggle="dropdown" 
-           aria-expanded="false">
-            =
-        </a>
-
-    <ul class="dropdown-menu" 
-                aria-labelledby="dropdownMenuLink">
-                <li><a class="dropdown-item" href="signin.html "  >
-                     signIn
-                    </a>
-                </li>
-                <li><a class="dropdown-item" href="signUp.html">
-                        signUp
-                    </a>
-                </li>
-              
-            </ul>
-        </div>
-     </div>
+    <?php include_once("tempolates/nav.php"); ?>
+    <div class ="banner">
+        
     <h1 style="background-color: bisque;">Brief History About Restaurants</h1>
     <p>Dining facilities have a long history, dating back to the eighteenth century.
      The first restaurant believed to have opened in France in 1762, specialized in soups and other simple dishes (Walker, 2021). Although the restaurant wasn’t the first to offer a menu with a selection of options, 
