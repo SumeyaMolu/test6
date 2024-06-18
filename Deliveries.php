8c0fce36062d967bd2986c4506ff80c1dead27c4
SumeyaMolu <sumeya.molu@strathmore.edu>
2024-06-18 12:48:19 +0300

    Added a functionality

--- Deliveries.php
+++ Deliveries.php
@@ -1,61 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href=
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-            rel="stylesheet"
-            integrity=
-    "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
-            crossorigin="anonymous">
-        <script src=
-    "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src=
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity=
-    "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-            crossorigin="anonymous"></script>
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css"
-</head>
+<?php include_once("templates/headings.php"); ?>
 <body>
-    <div class="topnav"></div>
-    <a href="index.html">Home</a>
-    <a href="About.html">AboutUs</a>
-    <a href="Branches.html">Our Branches</a>
-    <a href="Deliveries.html">Deliveries</a>
-    <a href="Reviews.html">Reviews</a>
-    <a href="ContactUs.html">ContactUs</a>
-    <a href="Offers.html">CrazyOffers</a>
-    <a href="Box.html">Box</a>
-    <a href="People.html">People</a>
-    <a href="SignIn.html">Sign In</a>
+    <?php include_once("tempolates/nav.php"); ?>
+    <div class ="banner">
 
-    <div  class ="topnav-right">
-        <a class="btn btn-secondary dropdown-toggle" 
-           href="#" 
-           role="button" 
-           id="dropdownMenuLink"
-           data-bs-toggle="dropdown" 
-           aria-expanded="false">
-            =
-        </a>
-
-    <ul class="dropdown-menu" 
-                aria-labelledby="dropdownMenuLink">
-                <li><a class="dropdown-item" href="signin.html "  >
-                     signIn
-                    </a>
-                </li>
-                <li><a class="dropdown-item" href="signUp.html">
-                        signUp
-                    </a>
-                </li>
-              
-            </ul>
-        </div>
-     </div>
 
     <h1>We Do Deliveries!!!!</h1>
     <p>In  an  uncommon  situation  like  this  coronavirus pandemic,  people  more  consider  their  health. 
