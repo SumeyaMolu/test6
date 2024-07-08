ac4373b2e53bc8283459e84934293b3677a66c01
SumeyaMolu <sumeya.molu@strathmore.edu>
2024-07-08 21:47:30 +0300

    Added a functionality

---
+++ templates/constants.php
@@ -0,0 +1,9 @@
+<?php
+//create php contants to enable connection to MYSQL database
+
+//create constants
+
+define("HOSTNAME", "localhost");
+define("HOSTUSER","root");
+define("HOSTPASS", "");
+define("DBNAME","test6");
